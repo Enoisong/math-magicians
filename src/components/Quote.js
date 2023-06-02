3d69be8d88249abf2d768783bbb8f42146e7aed1
Enoisong <enobongitiaba988@gmail.com>
2023-06-02 14:49:16 -0700

    made changes as required by Reviewer

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,24 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-class Quote extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      message: 'The will of man is his happiness. -Friedrich Schiller',
+function Quote() {
+  const [quote, setQuote] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://api.api-ninjas.com/v1/quotes?category=dreams', {
+          headers: {
+            'X-Api-Key': 'aDHnlIAfcVlR6A4XKlfcJw==XH3tzifPVIXYydwo',
+          },
+        });
+
+        const { quote } = response.data[0];
+        setQuote(quote);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
     };
-  }
 
-  render() {
-    const { message } = this.state;
+    fetchData();
+  }, []);
+
+  if (loading) {
     return (
-      <div className="quote">
-        <h1>Quote</h1>
-        <div className="content">
-          <p>{message}</p>
-        </div>
+      <div className="loaders">
+        Loading
+        <i className="fa fa-spinner" aria-hidden="true" />
       </div>
     );
   }
-}
 
+  if (error) {
+    return (
+      <div>
+        Error:
+        {error.message}
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h2 className="Quote-head">Quote:</h2>
+      <p className="Quote-body">{quote}</p>
+
+    </div>
+  );
+}
 export default Quote;
